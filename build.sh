6b72a8d0a572add9039a7ff17d60d9372b0333ee
alixian <xianguo@maihaoche.com>
2017-10-13 17:07:13 +0800

    add docker

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+PROJECT_NAME=domo
+PROJECT_HOME=/home/admin/app/
+JAR_DIR=${PROJECT_NAME}-core/target
+JAR_NAME=${PROJECT_NAME}.jar
+cd ${PROJECT_HOME}
+ls
+mvn clean install -Dmaven.test.skip&& mvn package -Dmaven.test.skip
+cd ${JAR_DIR}
+DOMO_PID=`ps -ef | grep "DOMO" | grep -v grep | awk '{print $2}'`
+echo "DOMO PID = $DOMO_PID"
+#kill -9 DOMO_PID
+java -jar ${JAR_NAME} --spring.profiles.active=#{EVN} --server.port=#{SERVER_PORT}
+echo "========Build successful======="
