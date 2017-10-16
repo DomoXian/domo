4a54a9dc42b89c9a699389f44ee8a1accf2968d2
李贤 <xianguo@maihaoche.com>
2017-10-16 16:41:26 +0800

    change build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 PROJECT_HOME=/home/admin/app/
+JAVA_OPTIONS="
+    -server -Xms1g -Xmx1g -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=512m -Xmn512M
+    -XX:+UseConcMarkSweepGC -XX:CMSMaxAbortablePrecleanTime=5000
+    -XX:+CMSClassUnloadingEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=80
+    -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/home/admin/app/logs/domo/online/java.hprof
+    -verbose:gc -Xloggc:/home/admin/app/logs/domo/online/gc.log -XX:+PrintGCDetails
+    -XX:+PrintGCDateStamps -Djava.awt.headless=true -Dsun.net.client.defaultConnectTimeout=10000
+    -Dsun.net.client.defaultReadTimeout=30000 -XX:+DisableExplicitGC -Xdebug
+    -agentlib:jdwp=transport=dt_socket,address=7080,server=y,suspend=n"
+
 JAR_DIR=${PROJECT_NAME}-core/target
 JAR_NAME=${PROJECT_NAME}.jar
 cd ${PROJECT_HOME}
 ls
 mvn clean install -Dmaven.test.skip&& mvn package -Dmaven.test.skip
 cd ${JAR_DIR}
-DOMO_PID=`ps -ef | grep "DOMO" | grep -v grep | awk '{print $2}'`
+DOMO_PID=`ps -ef | grep "domo" | grep -v grep | awk '{print $2}'`
 echo "DOMO PID = $DOMO_PID"
+echo "java_options = $JAVA_OPTIONS"
 #kill -9 DOMO_PID
-java ${JAVA_OPTIONS} -jar ${JAR_NAME} --spring.profiles.active=${ENV} --server.port=${SERVER_PORT}
+java ${JAVA_OPTIONS} -jar ${JAR_NAME} --spring.profiles.active=${ENV}
 echo "========Build successful======="
